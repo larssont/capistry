fc3848f5f6362bde6ebe346a63f7d777bd306560
Tommy Larsson <45052383+larssont@users.noreply.github.com>
2025-06-12 02:34:51 +0200

    Initial commit

---
+++ examples/filleting/main.py
@@ -0,0 +1,61 @@
+# ruff: noqa: F403, F405
+
+import logging
+from pathlib import Path
+
+from build123d import *
+from ocp_vscode import *
+
+from capistry import *
+
+init_logger(level=logging.INFO)
+
+# Camera defaults for ocp_vscode
+set_defaults(
+    reset_camera=Camera.KEEP,
+    default_color=(10, 125, 100),
+    default_edgecolor=(70, 55, 55),
+    default_opacity=0.75,
+    transparent=True,
+    metalness=0.50,
+    roughness=0.45,
+)
+
+width = 18
+length = 18
+wall = 1.25
+roof = 1.25
+height = 5
+gap = 1
+angle = -7
+
+stem = ChocStem()
+taper = Taper.uniform(7)
+
+c1 = RectangularCap(
+    width=width,
+    length=length,
+    height=height,
+    wall=wall,
+    roof=roof,
+    taper=taper,
+    stem=stem,
+    fillet_strategy=FilletUniform(),
+)
+
+c2 = c1.clone()
+c2.fillet_strategy = FilletSidesFirst()
+c2.build()
+c2.locate(c1.top_right * Pos(X=gap))
+
+c3 = c1.clone()
+c3.fillet_strategy = FilletSidesLast()
+c3.build()
+c3.locate(c2.top_right * Pos(X=gap))
+
+caps = [c1.compound, c2.compound, c3.compound]
+
+show(*caps)
+
+
+export_stl(Compound(caps), file_path=Path(__file__).parent / "caps.stl")
